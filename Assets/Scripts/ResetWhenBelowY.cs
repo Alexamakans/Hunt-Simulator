e0b54a8a11a5d5cb64504e3a0411d04680b6220f
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-05-02 13:42:50 +0200

    Added reset visual

--- Assets/Scripts/ResetWhenBelowY.cs
+++ Assets/Scripts/ResetWhenBelowY.cs
@@ -6,10 +6,12 @@ public class ResetWhenBelowY : MonoBehaviour
     public Vector3 resetTo = Vector3.up * 10;
     public Vector3 resetRandomRegionSize = new Vector3(10, 0, 10);
     public Rigidbody body;
+    public ResetPlayerVisual visual;
 
     void Reset()
     {
         body = GetComponentInChildren<Rigidbody>();
+        visual = GetComponentInChildren<ResetPlayerVisual>();
     }
 
     void OnEnable()
@@ -17,6 +19,12 @@ public class ResetWhenBelowY : MonoBehaviour
         if (!body)
         {
             Debug.LogWarning($"Cannot reset when below Y without '{nameof(body)}' set to a Rigidbody.", this);
+            enabled = false;
+        }
+
+        if (!visual)
+        {
+            Debug.LogWarning($"Cannot reset when below Y without '{nameof(visual)}' set to a {nameof(ResetPlayerVisual)}.", this);
             enabled = false;
         }
     }
@@ -32,16 +40,32 @@ public class ResetWhenBelowY : MonoBehaviour
 
     void Update()
     {
+        if (visual.isPlaying)
+        {
+            return;
+        }
+
         if (transform.position.y < minY)
         {
-            var halfSize = resetRandomRegionSize * 0.5f;
-            body.position = resetTo + new Vector3(
-                Random.Range(-halfSize.x, halfSize.x),
-                Random.Range(-halfSize.y, halfSize.y),
-                Random.Range(-halfSize.z, halfSize.z)
-            );
-            body.velocity = Vector3.zero;
-            body.angularVelocity = Vector3.zero;
+            visual.StartFading(this, nameof(ResetPosition));
         }
+    }
+
+    public void ResetPosition()
+    {
+        if (!body)
+        {
+            Debug.LogWarning($"Cannot reset when below Y without '{nameof(body)}' set to a Rigidbody.", this);
+            return;
+        }
+
+        var halfSize = resetRandomRegionSize * 0.5f;
+        body.position = resetTo + new Vector3(
+            Random.Range(-halfSize.x, halfSize.x),
+            Random.Range(-halfSize.y, halfSize.y),
+            Random.Range(-halfSize.z, halfSize.z)
+        );
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
     }
 }
