aeaffb79c3e9e5d4eb959799269b97c20adf7b24
Alexamakans <fougner.alexander@gmail.com>
2021-05-02 21:36:14 +0200

    Changed PlayerShooting component to use the new PickupAmmunition component

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public GameObject projectilePrefab;
     public Transform fireFrom;
     public float fireForce = 1;
+
+    public PickupAmmunition pickupAmmunition;
+
+    void Reset()
+    {
+        pickupAmmunition = GetComponent<PickupAmmunition>();
+    }
 
     void Update()
     {
@@ -16,28 +22,26 @@ public class PlayerShooting : MonoBehaviour
 
     void FireProjectile()
     {
-        if (!projectilePrefab)
-        {
-            Debug.LogWarning($"Cannot fire projectile: {nameof(projectilePrefab)} is unset.", this);
-            return;
-        }
-
         if (!fireFrom)
         {
             Debug.LogWarning($"Cannot fire projectile: {nameof(fireFrom)} is unset.", this);
             return;
         }
 
-        var clone = Instantiate(projectilePrefab, fireFrom.position, fireFrom.rotation);
-        var body = clone.GetComponentInChildren<Rigidbody>();
+        if (!pickupAmmunition.ammunition)
+        {
+            return;
+        }
 
-        if (!body)
+        var ammoBody = pickupAmmunition.ammunition.GetComponent<Rigidbody>();
+        if (!ammoBody)
         {
-            Debug.LogWarning("Projectile prefab does not have a RigidBody.", this);
+            Debug.LogWarning("Projectile does not have a Rigidbody.", this);
+            return;
         }
-        else
-        {
-            body.AddForce(fireFrom.forward * fireForce);
-        }
+
+        ammoBody.transform.SetPositionAndRotation(fireFrom.position, fireFrom.rotation);
+        ammoBody.AddForce(fireFrom.forward * fireForce);
+        pickupAmmunition.OnShoot();
     }
 }
