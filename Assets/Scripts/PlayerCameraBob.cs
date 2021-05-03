8ba01650aae06e61c9b604f60c3e5f83101e5465
Alexamakans <fougner.alexander@gmail.com>
2021-05-03 21:35:49 +0200

    Renamed HeadBob -> PlayerCameraBob

--- Assets/Scripts/PlayerCameraBob.cs
+++ Assets/Scripts/PlayerCameraBob.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadBob : MonoBehaviour
+public class PlayerCameraBob : MonoBehaviour
 {
     public FpsController fpsController;
 
