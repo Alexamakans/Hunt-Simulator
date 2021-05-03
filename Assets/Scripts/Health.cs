073c086237b5204c90cb4ddc261929e5464e0c99
Alexamakans <fougner.alexander@gmail.com>
2021-05-03 09:32:07 +0200

    Created Health component and IDeathListener interface

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(IDeathListener))]
+public class Health : MonoBehaviour
+{
+    public int health;
+    public bool isDead => health <= 0;
+
+    public void Damage(int amount)
+    {
+        health -= amount;
+        if (isDead)
+        {
+            var listeners = GetComponentsInChildren<IDeathListener>();
+            foreach (var listener in listeners)
+            {
+                listener.OnDeath();
+            }
+        }
+    }
+}
