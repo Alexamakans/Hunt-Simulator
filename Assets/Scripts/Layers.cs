6b78c40a82f9eef801f68c55c4ebb23c141b0fd5
Alexamakans <fougner.alexander@gmail.com>
2021-05-03 09:30:49 +0200

    Created layer constants

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,11 @@
+public static class Layers
+{
+    public const string DEFAULT = "Default";
+    public const string TRANSPARENT_FX = "TransparentFX";
+    public const string IGNORE_RAYCAST = "Ignore Raycast";
+    public const string PLAYER = "Player";
+    public const string WATER = "Water";
+    public const string UI = "UI";
+    public const string HANDS = "Hands";
+    public const string IGNORE_PLAYER = "Ignore Player";
+}
